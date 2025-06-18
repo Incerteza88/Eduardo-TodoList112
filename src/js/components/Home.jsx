e24a5e62e872e4a5fc269ab885b00bdbfe93cbe4
Incerteza88 <info@incerteza.es>
2025-06-18 10:04:04 +0000

    Lista de Tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,7 +6,7 @@ import TodoList from "./TodoList";
 function Home() {
 	return (
 		<div className="p-8 min-h-screen bg-gray-100">
-			<h1 className="text-2xl font-bold text-center mb-6">Mi Lista de Tareas</h1>
+			<h1 className="text-2xl font-bold text-center mb-6">Lista de Tareas</h1>
 			<TodoList />
 		</div>
 	);
--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -14,35 +14,40 @@ function TodoList() {
   }
 
   const click = (index) => {
-    const newTareas = tareas.filter((tarea,i) => index != i)
-    setTareas(newTareas)
+    // const newTareas = tareas.filter((tarea, i) => index != i)
+    setTareas(tareas.filter((_, i) => index != i))
   }
 
   return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3"></div>
+        <ul className="list-group col-6">
+          <li className="list-group-item active">
+            <input className="w-100" type="text" onChange={input} onKeyDown={keyDown} value={nuevaTarea} />
+          </li>
+          {tareas.map((tarea, index) => {
+            return (
+              <div>
+                <li className="list-group-item d-flex justify-content-between" key={index}>
+                  {tarea}
 
-    <div>
-      <input type="text" onChange={input} onKeyDown={keyDown} />
-      <ul>
-        {tareas.map((tarea, index) => {
-          return (
-            <li key={tarea}>
-              {tarea}
-              <button onClick={() => click(index)}>Borrar</button>
-            </li>
-          )
-        })}
-      </ul>
+                  <button className="btn btn-danger" onClick={() => click(index)}>Borrar</button>
+                </li>
+              </div>
+            )
+          })}
+
+          <li className="list-group-item">
+            {`${tareas.length} item `}
+          </li>
+
+        </ul>
+        <div className="col-3"></div>
+      </div>
+
     </div>
   )
-
-
-
-
-  // e.key === "Enter" && nuevaTarea.trim() !== ""
-  //   ? (setTareas([...tareas, nuevaTarea.trim()]), setNuevaTarea(""))
-  //   : null;
-
-
 
 }
 
